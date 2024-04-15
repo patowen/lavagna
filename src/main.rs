63368a0a5b3fb01f213baa58160f52038530e8de
Patrick Owen <patowen95@gmail.com>
2024-04-15 18:33:16 -0400

    Try to create minimal reproducible example of STATUS_ENTRYPOINT_NOT_FOUND

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
-#![deny(clippy::all)]
-#![forbid(unsafe_code)]
 // Disable the Windows Console
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use lavagna::*;
-
 fn main() {
-    // This is needed, otherwise bevy logs are not shown on browser console
-    #[cfg(target_arch = "wasm32")]
-    lavagna::web::setup_log();
-
-    run(lavagna::options())
+    println!("Hello world");
 }
